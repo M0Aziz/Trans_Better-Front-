71f7ab2a782a6fed649b2a7273e5a03b3e84a8fd
M0Aziz <medaziz.chouchi@esprit.tn>
2023-10-21 16:16:37 +0200

    Modification

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -14,16 +14,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'maruti-fasterners'`, () => {
+  it(`should have as title 'Transbetter'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('maruti-fasterners');
+    expect(app.title).toEqual('Transbetter');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('maruti-fasterners app is running!');
+    expect(compiled.querySelector('.content span')?.textContent).toContain('Transbetter app is running!');
   });
 });
